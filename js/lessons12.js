4896247238e2ccd45be74cb2ac94af9dd4634faa
Sasha <dimurchik@ukr.net>
2013-11-25 20:31:14 +0200

    Методы комбаины 2

--- js/lessons12.js
+++ js/lessons12.js
@@ -6,6 +6,10 @@
 	var input = $('input');
 
 	console.log(input.prop({value: 'asd', title: 'edf'}).append());
+	console.log(input.prop('title', function(index, value){
+		console.log(this.value);
+		console.log(arguments);
+	}));
 
 
 })(jQuery)
